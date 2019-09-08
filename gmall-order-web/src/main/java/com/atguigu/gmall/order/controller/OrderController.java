9a71c93e140414436a0f3192020b6c45c27aa577
zibinwang <960142717@qq.com>
2019-09-08 13:48:47 +0800

    增加order模块

--- gmall-order-web/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
+++ gmall-order-web/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -1,7 +1,33 @@
 package com.atguigu.gmall.order.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.UserInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
+    @Reference
+    private UserInfoService userInfoService;
+
+    @RequestMapping("trade")
+    //页面转跳
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("list")
+    @ResponseBody
+    public List<UserAddress> trade(String userId){
+        //返回json数据
+        return userInfoService.getUserAddressList(userId);
+    }
+
+
+
 }
